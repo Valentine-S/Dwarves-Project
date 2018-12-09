6585e16ef300807bde3a3174d3c2647c922a7701
Valentine-s <valentineshidlovskiy@gmail.com>
2018-12-09 01:36:01 -0500

    Updated

--- Project 3/dwarvesC/bot.cpp
+++ Project 3/dwarvesC/bot.cpp
@@ -55,30 +55,24 @@ void onStart(int rows, int cols, int num, std::ostream &log) {
   NUM = num;
 
   if(NUM == 6){
-    sectorCols[0] = 1/3;
-    sectorCols[1] = 2/3;
-    sectorCols[2] = 1;
-    sectorCols[3] = 1/3;
-    sectorCols[4] = 2/3;
-    sectorCols[5] = 1; 
+    sectorCols[0] = 0;
+    sectorCols[1] = 1.0/3;
+    sectorCols[2] = 2.0/3;
+    sectorCols[3] = 1;
+
   }
   if(NUM == 7){
-    sectorCols[0] = 1/3;
-    sectorCols[1] = 2/3;
-    sectorCols[2] = 1;
-    sectorCols[3] = 1/3;
-    sectorCols[4] = 2/3;
-    sectorCols[5] = 1; 
+    sectorCols[0] = 0;
+    sectorCols[1] = 1.0/3;
+    sectorCols[2] = 2.0/3;
+    sectorCols[3] = 1;
   }
   if(NUM == 8){
-    sectorCols[0] = 1/4;
-    sectorCols[1] = 1/2;
-    sectorCols[2] = 3/4;
-    sectorCols[3] = 1;
-    sectorCols[4] = 1/4;
-    sectorCols[5] = 1/2;
-    sectorCols[6] = 3/4;
-    sectorCols[7] = 1;
+    sectorCols[0] = 0;
+    sectorCols[1] = 1.0/4;
+    sectorCols[2] = 1.0/2;
+    sectorCols[3] = 3.0/4;
+    sectorCols[4] = 1;
   }
   
 }
@@ -94,50 +88,63 @@ void onStart(int rows, int cols, int num, std::ostream &log) {
    log:     a cout-like log  */
 
 void onAction(Dwarf &dwarf, int day, int hours, int minutes, ostream &log) {
-  // Get current position of the dwarf
-  int r = dwarf.row();
-  int c = dwarf.col();
+  changingRow = 0;
+
+  if(walked[dwarf.name()] == true){
+      log << "Chop" << " Name: " << dwarf.name() << endl;
+      dwarf.start_chop(dirs[dwarf.name()]);
+      walked[dwarf.name()] = false;
+      return;
+}
 
   if(day < 3){
     if(dwarf.name() > (NUM/2 -1)){
       changingRow = 0.5;
     }
 
-    if(walked[dwarf.name()] == false){
-      //log << "Test1" << endl;
+    
       for(int row = changingRow * ROWS; row < (int)(ROWS * (0.5 + changingRow)); row++){//Tell dwarfs where to walk to get PINE_TREE
 	for(int col = (int)(sectorCols[dwarf.name()%(NUM/2)] * COLS); col < (int) (sectorCols[dwarf.name()%(NUM/2) + 1] * COLS); col++){
-	 
-	  if(isNextToAPineTree(dwarf,row,col) == true){
+	log << dwarf.name() << " " << changingRow * ROWS << " " << (int)(ROWS * (0.5 + changingRow)) << "\n";
+	log << dwarf.name() << " " << (int)(sectorCols[dwarf.name()%(NUM/2)] * COLS) << " " << (int) (sectorCols[dwarf.name()%(NUM/2) + 1] * COLS) << "\n";
+	if(dwarf.look(row,col) == EMPTY){
 	   
-	    if(dwarf.look(r,c+1) == PINE_TREE){
-	      dirs[dwarf.name()] == EAST;
+	    if(dwarf.look(row,col+1) == PINE_TREE){
+	      dirs[dwarf.name()] = EAST;
+              log << "Found tree -- walking NAME:" << dwarf.name() << endl;
+	      walked[dwarf.name()] = true;
+	      dwarf.start_walk(row,col);
+	      
+	      return;
 	    }
-	    if(dwarf.look(r+1,c) == PINE_TREE){
-	      dirs[dwarf.name()] == SOUTH;
+	    if(dwarf.look(row+1,col) == PINE_TREE){
+	      dirs[dwarf.name()] = SOUTH;
+	      log << "Found tree -- walking NAME:" << dwarf.name() << endl;
+	      walked[dwarf.name()] = true;
+	      dwarf.start_walk(row,col);
+	      
+	      return;
 	    }
-	    if(dwarf.look(r-1,c) == PINE_TREE){
-	      dirs[dwarf.name()] == NORTH;
+	    if(dwarf.look(row-1,col) == PINE_TREE){
+	      dirs[dwarf.name()] = NORTH;
+	      log << "Found tree -- walking NAME:" << dwarf.name() << endl;
+	      walked[dwarf.name()] = true;
+	      dwarf.start_walk(row,col);
+	      
+	      return;
 	    }
-	    if(dwarf.look(r,c-1) == PINE_TREE){
-	      dirs[dwarf.name()] == WEST;
+	    if(dwarf.look(row,col-1) == PINE_TREE){
+	      dirs[dwarf.name()] = WEST;
+	      log << "Found tree -- walking NAME:" << dwarf.name() << endl;
+	      walked[dwarf.name()] = true;
+	      dwarf.start_walk(row,col);
+	     
+	      return;
 	    }
-	    
-	    log << "Found tree -- walking NAME:" << dwarf.name() << endl;
-	    dwarf.start_walk(row,col);
-	    walked[dwarf.name()] = true;
-	    return;
 	  }
 	}
       }
-    }
-    else{//if walked is true
-      
-      log << "Chop" << " Name: " << dwarf.name() << endl;
-      dwarf.start_chop(dirs[dwarf.name()]);
-      walked[dwarf.name()] = false;
-      return;
-    }
+      //}
   }
 	    
 	    
